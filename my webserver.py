0dd13a5c4eb30d751e7d829b16cf17173d4fec15
naveen2003-21 <94387019+naveen2003-21@users.noreply.github.com>
2021-11-23 14:36:40 +0530

    added image

--- my webserver.py
+++ my webserver.py
@@ -1,21 +1,3 @@
-# Developing a Simple Webserver
-## AIM:
-To develop a simple webserver to display about top five programming languages.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation
-### Step 2:
-Design of webserver workflow
-### Step 3:
-Implementation using Python code
-### Step 4:
-Serving the HTML pages.
-### Step 5:
-Testing the webserver
-
-## PROGRAM:
-```
 from http.server import HTTPServer, BaseHTTPRequestHandler
 content = """
 <!DOCTYPE html>
@@ -60,11 +42,3 @@ server_address = ('',8000)
 httpd = HTTPServer(server_address,myhandler)
 print("my webserver is running...")
 httpd.serve_forever()
-```
-
-## OUTPUT:
-![Client side Output](./Clientside.png)
-![Server side](./Serverside1.png)
-![Server side 2](./Serverside2.png)
-## RESULT:
-Thus, a simple webserver is created to about display top five programming languages.
